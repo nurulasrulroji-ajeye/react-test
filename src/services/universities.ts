cf722badab038ab23b3e04ea736bb1fd809b4af8
nurulasrulroji-ajeye <asrulrojin@gmail.com>
2023-12-11 22:16:33 +0700

    try fix 3

--- src/services/universities.ts
+++ src/services/universities.ts
@@ -2,7 +2,7 @@ import connection from "../config";
 
 export class UniversitiesServices {
     async getAllUniv() {
-        const { data } = await connection.get("/search?country=indonesia&limit=10/")
+        const { data } = await connection.get("/search?country=indonesia&limit=10")
         return data
     }
 }
